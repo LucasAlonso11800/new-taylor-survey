84cea51a0e3c16241f767aec202e18f49850237c
LucasAlonso11800 <lucasalonso11800@gmail.com>
2022-11-04 18:37:05 -0300

    Added route for getting question set

--- backend/app.py
+++ backend/app.py
@@ -14,16 +14,33 @@ def get_db():
         db.close()
 
 # Get question set with questions and their options
+@app.get('/question-set/{order}')
+def get_question_set(order: int, db: Session = Depends(get_db)):
+    questionSet = db.query(models.QuestionSet).where(models.QuestionSet.question_set_order == order).first()
+    if questionSet is None:
+        return { "error": True, "message": "Question set not found" }
+
+    questions = db.query(models.Question.question_id, models.Question.question_text)\
+                    .where(models.Question.question_id == models.QuestionSet.question_set_id)\
+                    .all()
+    options = db.query(models.Option.option_id, models.Option.option_text)\
+                .where(models.Option.option_question_set_id == models.QuestionSet.question_set_id)\
+                .all()
+
+    return { "error": False, 
+            "message": "", 
+            "data": { "questionSet": questionSet, "questions": questions, "options": options }
+            }
 
 # Insert answer
 @app.post('/answer')
 def insert_answer(answer: schemas.Answer, db: Session = Depends(get_db)):
-    question = db.query(models.Question).filter(models.Question.question_id == answer.answer_question_id).first()
-    option = db.query(models.Option).filter(models.Option.option_id == answer.answer_option_id).first()
+    question = db.query(models.Question).where(models.Question.question_id == answer.answer_question_id).first()
+    option = db.query(models.Option).where(models.Option.option_id == answer.answer_option_id).first()
 
-    if question == None:
+    if question is None:
         return { "error": True, "message": "Question not found" }
-    elif option == None:
+    elif option is None:
         return { "error": True, "message": "Option not found" }
     else:
         newAnswer = models.Answer(
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from dbconfig import Base
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 class QuestionSet(Base):
     __tablename__ = "questions_set"
@@ -8,20 +9,23 @@ class QuestionSet(Base):
     question_set_title = Column(String, unique=True, index=True)
     question_set_order = Column(Integer, unique=True, index=True)
 
+    questions = relationship("Question")
+    options = relationship("Option")
+
 class Question(Base):
     __tablename__ = "questions"
 
     question_id = Column(Integer, primary_key=True, index=True)
     question_text = Column(String)
     question_question_set_id = Column(Integer, ForeignKey("questions_set.question_set_id"))
-
+    
 class Option(Base):
     __tablename__ = "options"
 
     option_id = Column(Integer, primary_key=True, index=True)
     option_text = Column(String)
     option_question_set_id = Column(Integer, ForeignKey("questions_set.question_set_id"))
-
+    
 class Answer(Base):
     __tablename__ = "answers"
     
